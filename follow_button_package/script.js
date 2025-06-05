8f33813168d9afb94127a1ae2bdb6e8772dca722
skrrpture <skrrpture@gmail.com>
2025-06-05 19:32:51 +0200

    upload final portfolio

--- follow_button_package/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const container = document.querySelector(".follow-container");
-  const toggle = document.querySelector(".follow-toggle");
-
-  toggle.addEventListener("click", function () {
-    container.classList.toggle("active");
-  });
-});
